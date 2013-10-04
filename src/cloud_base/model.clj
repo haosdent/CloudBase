efe0f760bc29aa1807eee9a0c50916a0670b381e
haosdent <haosdent@gmail.com>
2013-10-04 15:32:12 +0800

    Impl renderable protocol on hashmap.

--- src/cloud_base/model.clj
+++ src/cloud_base/model.clj
@@ -1,15 +1,9 @@
 (ns cloud-base.model
-  (:require [cheshire.core :refer :all])  
   (:refer-clojure :exclude [get])
-  (:import [cloud_base DB]))
-
-(defmacro to-json [map]
-  `{:status 200
-    :headers {"Content-Type" "application/json; charset=utf-8"}
-    :body (generate-string ~map)}) 
+  (:import [cloud_base DB])) 
 
 (defn get [table row version]
-  (to-json (DB/get table row version)))  
+  (DB/get table row version))  
 
 (defn put [table row version data]
   (DB/put table row version data)) 
--- src/cloud_base/route.clj
+++ src/cloud_base/route.clj
@@ -1,9 +1,19 @@
 (ns cloud-base.route
   (:use [compojure.route :only [files not-found]]
         [compojure.handler :only [site]]
+        [compojure.response :only [Renderable]]
         [compojure.core :only [defroutes GET POST DELETE PUT ANY context]]
+        [ring.util.response :only (response content-type)]
+        cheshire.core
         org.httpkit.server)
-  (:require [cloud-base.model :as model])) 
+  (:require [cloud-base.model :as model])
+  (:import [java.util HashMap]))  
+
+(extend-protocol Renderable
+  HashMap
+  (render [map _]
+    (-> (response (generate-string map))
+        (content-type "application/json; charset=utf-8"))))
 
 (defroutes all-routes
   (GET  "/:table/:row/:version" [] 
@@ -21,6 +31,8 @@
 
   (POST "/:table"               []
         #(model/create 
-          (-> % :params :table))))   
+          (-> % :params :table)))
+
+  (not-found "404"))    
 
 
