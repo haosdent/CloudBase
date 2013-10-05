b33c8fac0010151707a4fca3ae2578aa9442bfd0
haosdent <haosdent@gmail.com>
2013-10-05 17:14:42 +0800

    Update example.

--- examples/web/js/color.js
+++ examples/web/js/color.js
@@ -1,6 +1,16 @@
-client = 'haosdent'
 var app = new App('hello');
+
 var cb = function(){
-    $('body').text('Model A:' + this.a);
+    $('#point').text('Click Count:' + this.count);
 };
+
 var model = app.create('one', 'hi', cb);
+$('#point').click(function(){
+    if(model.count !== undefined){
+        model.count++;
+        model.save();
+    }else{
+        model.count = 0;
+        model.save();
+    };
+});
