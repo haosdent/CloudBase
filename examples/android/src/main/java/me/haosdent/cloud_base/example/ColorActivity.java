e94a84a6dfefc0c4f3b734c669837166a4f6de3b
haosdent <haosdent@gmail.com>
2013-11-18 23:44:39 +0800

    Use color-picker-view in android example.

---
+++ examples/android/src/main/java/me/haosdent/cloud_base/example/ColorActivity.java
@@ -0,0 +1,13 @@
+package me.haosdent.cloud_base.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ColorActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_color);
+  }
+}
