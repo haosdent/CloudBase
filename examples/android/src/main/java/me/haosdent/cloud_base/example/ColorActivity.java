b7c023cab4077a405442143a7d0a862d4ea2f956
haosdent <haosdent@gmail.com>
2013-12-10 02:36:59 +0800

    Use PreferenceActivity instead of Activity.

--- examples/android/src/main/java/me/haosdent/cloud_base/example/ColorActivity.java
+++ examples/android/src/main/java/me/haosdent/cloud_base/example/ColorActivity.java
@@ -1,13 +1,28 @@
 package me.haosdent.cloud_base.example;
 
+import afzkl.development.colorpickerview.dialog.ColorPickerDialog;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
-public class ColorActivity extends Activity {
+public class ColorActivity extends PreferenceActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_color);
+    getFragmentManager().beginTransaction()
+        .replace(android.R.id.content, new ColorFragment()).commit();
+  }
+
+  public static class ColorFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.main);
+    }
   }
 }
